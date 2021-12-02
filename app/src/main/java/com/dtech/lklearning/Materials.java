13c4928a3caed413779619c6ca7f95e2598a6478
raghibrehan <raghibrehan1@gmail.com>
2021-12-02 15:35:55 +0530

    updated feedback page

---
+++ app/src/main/java/com/dtech/lklearning/Materials.java
@@ -0,0 +1,14 @@
+package com.dtech.lklearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Materials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materials);
+    }
+}
